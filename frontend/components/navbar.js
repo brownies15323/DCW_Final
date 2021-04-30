48166776f4c27a52b14c1ef58a3cdc4cf55f5c99
brownies15323 <32190081+brownies15323@users.noreply.github.com>
2021-04-30 10:47:30 +0700

    edit page

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => (
     <div class= "head">
         <div>
         <Link href="/"><a> Home </a></Link> |
-        <Link href="/register"><a> Register </a></Link>  |
         <Link href="/profile"><a> Profile </a></Link> |  
         <Link href="/getConfig"><a> Config </a></Link> |
+        <Link href="/register"><a> Register </a></Link>  |
         <Link href="/login"><a> Login </a></Link> |
         <Link href="/logout"><a> Logout </a></Link> 
         </div>
